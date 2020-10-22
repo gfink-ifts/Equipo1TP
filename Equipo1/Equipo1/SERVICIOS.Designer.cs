ab852cec60bac534c0d5110ba821b9e03ed2e74c
1746@ifts24.edu.ar <1746@ifts24.edu.ar>
2020-10-22 18:10:19 -0300

    Testing2

--- Equipo1/Equipo1/SERVICIOS.Designer.cs
+++ Equipo1/Equipo1/SERVICIOS.Designer.cs
@@ -28,7 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(391, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // SERVICIOS
             // 
@@ -36,6 +46,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSteelBlue;
             this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button1);
             this.ForeColor = System.Drawing.Color.MidnightBlue;
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "SERVICIOS";
@@ -45,5 +56,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
